d32320d12ffd0d3836f9dadfdbca75dc3a2f6ed8
Moaad Fattali <f.moaad@gmail.com>
2021-04-08 23:15:21 +0200

    add strip container

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import StripContainer from "./components/StripContainer";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <StripContainer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/StripContainer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./StripContainer.css";
+
+const StripContainer = () => {
+  const message = "blbalbala";
+
+  return <div className="container">{message}</div>;
+};
+
+export default StripContainer;
