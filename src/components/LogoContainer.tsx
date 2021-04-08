fac012131b02cc43f0fa1aca5ce930d223073417
Moaad Fattali <f.moaad@gmail.com>
2021-04-09 00:24:26 +0200

    delete logo and increase left margin

--- src/components/LogoContainer.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./LogoContainer.css";
-
-const LogoContainer = () => (
-  <img alt="logo hyris" className="logo" src={`${process.env.PUBLIC_URL}/logo.png`} />
-);
-
-export default LogoContainer;
--- src/components/StripContainer.tsx
+++ src/components/StripContainer.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import LogoContainer from "./LogoContainer";
 import Strip from "./Strip";
 import "./StripContainer.css";
 
 const StripContainer = () => (
   <div className="container">
-    <LogoContainer />
-    <div className="separator">|</div>
     <Strip />
   </div>
 );
