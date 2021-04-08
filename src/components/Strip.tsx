39f7681811a9178c29e19fa689666ec60688cd59
Moaad Fattali <f.moaad@gmail.com>
2021-04-09 00:48:30 +0200

    fmt messages

--- src/components/Strip.tsx
+++ src/components/Strip.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 import "./Strip.css";
 
-const Strip = () => {
-  const message =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+const messages = [
+  "Envoyez vos msg au 102910921",
+  "Lorem ipsum dolor sit amet",
+  "Consectetur adipiscing elit",
+  "sed do eiusmod tempor incididunt ut labore et ",
+  "dolore magna aliqua. Ut enim ad minim veniam",
+  "quis nostrud exercitation ullamco laboris nisi ut aliquip",
+  "ex ea commodo consequat. Duis aute irure dolor in reprehenderit i",
+  "n voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+  "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia",
+  "deserunt mollit anim id est laborum",
+];
 
-  return <div className="strip">{message.toUpperCase()}</div>;
-};
+const Strip = () => (
+  // const [scroll, setScroll] = useState(0);
+  <div className="strip">
+    {messages.map((message) => (
+      <>
+        <div className="message">{message.toUpperCase()}</div>
+        <div className="message">•</div>
+      </>
+    ))}
+  </div>
+);
 
 export default Strip;
