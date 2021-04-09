cdc147521603b132e5099d9ff32d8b7fba6a2b78
Moaad Fattali <f.moaad@gmail.com>
2021-04-09 10:47:07 +0200

    implement css transition

--- src/components/Strip.tsx
+++ src/components/Strip.tsx
@@ -5,25 +5,37 @@ const messages = [
   "Envoyez vos msg au 102910921",
   "Lorem ipsum dolor sit amet",
   "Consectetur adipiscing elit",
+  "deserunt mollit anim id est laborum",
   "sed do eiusmod tempor incididunt ut labore et ",
-  "dolore magna aliqua. Ut enim ad minim veniam",
+  "deserunt mollit anim id est labdazzdorum",
+  "Envoyez vos msg au 102910921212",
   "quis nostrud exercitation ullamco laboris nisi ut aliquip",
   "ex ea commodo consequat. Duis aute irure dolor in reprehenderit i",
+  "deserunt mollit anim id est laborzaddazdzaum",
   "n voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
   "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia",
-  "deserunt mollit anim id est laborum",
+  "deserunt mollit anim id est laboazdadzrum",
+  "Envoyez vos msg au 10291011921",
 ];
 
-const Strip = () => (
-  // const [scroll, setScroll] = useState(0);
-  <div className="strip">
-    {messages.map((message) => (
-      <>
-        <div className="message">{message.toUpperCase()}</div>
-        <div className="message">•</div>
-      </>
-    ))}
-  </div>
-);
+const Strip = () => {
+  const animationInfo = {
+    length: messages.join(",").length * 15,
+    duration: (messages.join(",").length * 15) / 80,
+  };
+
+  // eslint-disable-next-line
+  console.log(animationInfo);
+
+  return (
+    <div className="strip">
+      {messages.map((message) => (
+        <div className="message" key={message}>
+          <div>{message.toUpperCase()}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Strip;
